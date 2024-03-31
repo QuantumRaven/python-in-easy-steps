3023705db60133b8726defaa68a9f4c0611504c4
QuantumCrow <cybernetic-raven@protonmail.com>
2024-03-31 19:29:33 -0400

    add: last two lines added

--- ch2_performaing_operations/precedence.py
+++ ch2_performaing_operations/precedence.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.12
 
+# Setting precedence, pg. 36
+
 a = 2
 b = 4
 c = 8
@@ -7,5 +9,11 @@ c = 8
 print('\nDefault Order:\t', a, '*', c, '+', b, '=', a * c + b)
 print('\nForced Order:\t', a, '* (', c, '+', b, ') =', a * (c + b))
 
-print('\nDefault Order:\t', c, '//' b, '-', a, '=', c // b - a)
+print('\nDefault Order:\t', c, '//', b, '-', a, '=', c // b - a)
 print('Forced Order:\t', c, '// (', b, '-', a, ') =', c // (b - a))
+
+print('\nDefault Order:\t', c, '%', a, '+', b, '=', c % a + b)
+print('\nForced Order:\t', c, '% (', a, '+', b, ') =', c % (a + b))
+
+print('\nDefault Order', c, '**', a, '+', b, '=', c ** a + b)
+print('\nDefault Order', c, '**(', a, '+', b,') =', c ** (a + b))
