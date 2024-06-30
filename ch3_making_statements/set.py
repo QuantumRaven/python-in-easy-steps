de878d85e9fa1133bcf9618506394c37dd9f9fe0
QuantumRaven <adroid-raven.proton.me>
2024-06-30 05:24:17 -0400

    add: Add set.py

--- ch3_making_statements/set.py
+++ ch3_making_statements/set.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3.12
 
+zoo = ('Kangaroo', 'Leopard', 'Moose')
+print('Tuple: ', zoo, '\tLength: ', len(zoo))
+print(type(zoo))
 
+bag = {'Red', 'Green', 'Blue'}
+bag.add('Yellow')
+print('\nIs Green In bag Set?: ', 'Green' in bag)
+print('Is Orange in bag Set?: ', 'Ornage' in bag)
+
+box = {'Red', 'Purple', 'Yellow'}
+print('\nSet: ', box, '\t\tLength', len(box))
+print('Common To Both Sets:', bag.intersection(box))
 
